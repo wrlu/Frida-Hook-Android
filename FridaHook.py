cabee3895b222b2912969a1dd884835714a4f748
小路 <wrlus@outlook.com>
2019-08-23 14:20:55 +0800

    Update TLS Hook JS Code

--- FridaHook.py
+++ FridaHook.py
@@ -2,8 +2,8 @@ import sys
 import frida
 
 
-js_file_names = ['HwTLS', 'DanaleToken']
-process_name = 'com.huawei.ipc'
+js_file_names = ['HwTLS']
+process_name = 'com.huawei.himovie.tv'
 
 
 def on_message(message, data):
@@ -16,16 +16,21 @@ def on_message(message, data):
 
 
 if __name__ == '__main__':
-    print(frida.__version__)
-    manager = frida.get_device_manager()
-    devices = manager.enumerate_devices()
-    for ldevice in devices:
-        print(ldevice)
-    # 84B5T15B03006088: Huawei Nexus 6P
-    # device = manager.get_device('84B5T15B03006088')
-    # 8AXX11E93: Google Pixel 3
-    device = manager.get_device('8AXX11E93')
     try:
+        print(frida.__version__)
+        manager = frida.get_device_manager()
+        devices = manager.enumerate_devices()
+        for ldevice in devices:
+            print(ldevice)
+        # Huawei Nexus 6P
+        # device = manager.get_device('84B5T15B03006088')
+
+        # Google Pixel 3
+        # device = manager.get_device('8AXX11E93')
+
+        # Hisilicon OSCA-550
+        device = manager.get_device('192.168.137.97:5555')
+
         processes = device.enumerate_processes()
         for process in processes:
             print(process)
@@ -36,9 +41,11 @@ if __name__ == '__main__':
             script.on('message', on_message)
             script.load()
         sys.stdin.read()
-    except frida.InvalidArgumentError as error:
-        print(error)
-    except frida.ServerNotRunningError as error:
-        print(error)
+    except frida.InvalidArgumentError as e:
+        print('[Error] 参数错误(设备不存在): '+repr(e))
+    except frida.ProcessNotFoundError as e:
+        print('[Error] 进程不存在: '+repr(e))
+    except frida.ServerNotRunningError as e:
+        print('[Error] frida没有运行: '+repr(e))
 
 
--- HookHwTLSAndroid.js
+++ HookHwTLSAndroid.js
@@ -1,30 +1,92 @@
 setImmediate(function() {
     Java.perform(function() {
-//        okhttp3.x hook (original)
-//        Hook original okhttp3.CertificatePinner.check(String, List<Certificate>) function
+    /*
+        okhttp3.x hook
+    */
+//        Hook original okhttp3.CertificatePinner.check function
         try {
             var CertificatePinner = Java.use('okhttp3.CertificatePinner');
             console.log('OkHTTP 3.x Found');
             CertificatePinner.check.overload('java.lang.String', 'java.util.List').implementation = function () {
-                send('OkHTTP 3.x okhttp3.CertificatePinner.check() called. Not throwing an exception.');
+                send('OkHTTP 3.x CertificatePinner.check() called');
             }
-        } catch (err) {
-            if (err.message.indexOf('ClassNotFoundException') === 0) {
-                throw new Error(err);
-            }
+        } catch (e) {
+            console.log(e);
         }
-//        okhttp3.x hook (com.huawei.smarthome)
-//        Hook okhttp3.g.a(String, List<Certificate>) function
+//        Hook okhttp3.g.a function
         try {
             var CertificatePinner = Java.use('okhttp3.g');
-            console.log('OkHTTP 3.x Found');
+            console.log('OkHTTP 3.x (g.a mixed) Found');
             CertificatePinner.a.overload('java.lang.String', 'java.util.List').implementation = function () {
-                send('OkHTTP 3.x okhttp3.g.a() called. Not throwing an exception.');
+                send('OkHTTP 3.x g.a() called');
             }
-        } catch (err) {
-            if (err.message.indexOf('ClassNotFoundException') === 0) {
-                throw new Error(err);
-            }
+        } catch (e) {
+            console.log(e);
         }
+
+//        Hook okhttp3.d.a function
+        try {
+            var CertificatePinner = Java.use('okhttp3.d');
+            console.log('OkHTTP 3.x (d.a mixed) Found');
+            CertificatePinner.a.overload('java.lang.String', 'java.util.List').implementation = function () {
+                send('OkHTTP 3.x g.d() called');
+            }
+        } catch (e) {
+            console.log(e);
+        }
+
+    /*
+        Android 7.0+ TrustManagerImpl hook
+    */
+//        Hook com.android.org.conscrypt.TrustManagerImpl.verifyChain function
+        try {
+            var TrustManagerImpl = Java.use('com.android.org.conscrypt.TrustManagerImpl');
+            console.log('Android 7.0+ TrustManagerImpl Found');
+            TrustManagerImpl.verifyChain.implementation = function (untrustedChain, trustAnchorChain, host, clientAuth, ocspData, tlsSctData) {
+                send('Conscrypt TrustManagerImpl.verifyChain() called');
+                return untrustedChain;
+            }
+        } catch (e) {
+            console.log(e);
+        }
+
+    /*
+        JSSE hook
+    */
+        try {
+//        Hook javax.net.ssl.HttpsURLConnection.setDefaultHostnameVerifier() function
+            var HttpsURLConnection = Java.use('javax.net.ssl.HttpsURLConnection');
+            console.log('JSSE HttpsURLConnection Found');
+            HttpsURLConnection.setDefaultHostnameVerifier.implementation = function (hostnameVerifier) {
+                send('JSSE HttpsURLConnection.setDefaultHostnameVerifier() called');
+            };
+//        Hook javax.net.ssl.HttpsURLConnection.setSSLSocketFactory() function
+            HttpsURLConnection.setSSLSocketFactory.implementation = function (SSLSocketFactory) {
+                send('JSSE HttpsURLConnection.setSSLSocketFactory() called');
+            };
+//        Hook javax.net.ssl.HttpsURLConnection.setHostnameVerifier() function
+            HttpsURLConnection.setHostnameVerifier.implementation = function (hostnameVerifier){
+                send('JSSE HttpsURLConnection.setHostnameVerifier() called');
+            };
+        } catch (e) {
+            console.log(e);
+        }
+
+        try {
+            var DefaultHostnameVerifier = Java.use('javax.net.ssl.DefaultHostnameVerifier');
+            console.log('JSSE DefaultHostnameVerifier Found');
+//        Hook javax.net.ssl.DefaultHostnameVerifier.verify() function
+            DefaultHostnameVerifier.verify.overload('java.lang.String', 'javax.net.ssl.SSLSession').implementation = function (host, session) {
+                send('JSSE DefaultSSLSocketFactory.verify() called');
+                return true;
+            };
+            DefaultHostnameVerifier.verify.overload('java.lang.String', 'java.security.cert.X509Certificate').implementation = function (host, session) {
+                send('JSSE DefaultSSLSocketFactory.verify() called');
+                return true;
+            };
+        } catch (e) {
+            console.log(e);
+        }
+
     });
 });
