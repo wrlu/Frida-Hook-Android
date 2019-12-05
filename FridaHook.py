9e74832bedbe50e87c39683a64833d6d9e452ee5
路伟饶 <wrlus@outlook.com>
2019-12-05 17:15:12 +0800

    更改提示语为英文

--- FridaHook.py
+++ FridaHook.py
@@ -11,45 +11,47 @@ process_names = [
 
 def on_message(message, data):
     if message['type'] == 'send':
-        print('[接收消息] '+message['payload'])
+        print('[Send] '+message['payload'])
     elif message['type'] == 'error':
-        print('[错误] '+message['description'])
+        print('[Error] '+message['description'])
     else:
-        print('[错误] '+message)
+        print('[Error] '+message)
 
 
 if __name__ == '__main__':
     try:
-        print('[提示] 当前frida版本: '+str(frida.__version__))
+        print('[Info] Current frida version: '+str(frida.__version__))
         manager = frida.get_device_manager()
         devices = manager.enumerate_devices()
         for ldevice in devices:
-            print('[提示] 发现设备: '+str(ldevice))
+            print('[Info] Device discovered: '+str(ldevice))
         # Google Pixel
         # device = manager.get_device('FA74D0301125')
 
         # Huawei Nexus 6P
         device = manager.get_device('84B5T15B03006088')
 
-        print('[提示] 连接目标设备成功: '+str(device))
+        print('[Info] Connect to the target device successfully: '+str(device))
         front_app = device.get_frontmost_application()
-        print('[提示] 设备前台应用程序: '+str(front_app))
+        print('[Info] Front Application on the device: '+str(front_app))
         if front_app.identifier not in process_names:
-            print('[警告] 设备的前台应用程序和目标应用程序都不同( '+front_app.identifier+' 没有位于 '+str(process_names)+' )')
+            print('[Warning] Device front application is different from all the hooked application ( '+front_app.identifier+' != '+str(process_names)+' )')
         for process_name in process_names:
             process = device.attach(process_name)
             for js_file_name in js_file_names:
                 process_name_var = 'var _pname = "'+process_name+'";'
-                print('[提示] 读取注入脚本，名称: Hook' + js_file_name + 'Android.js')
+                print('[Info] Inject script name: ' + js_file_name + 'Android.js')
                 script = process.create_script(process_name_var + open('Hook' + js_file_name + 'Android.js').read())
                 script.on('message', on_message)
-                print('[提示] 加载注入脚本，名称: Hook' + js_file_name + 'Android.js')
+                print('[Info] Load script name: Hook' + js_file_name + 'Android.js')
                 script.load()
-        print('[提示] 开始等待注入脚本输出')
+        print('[Info] Waiting for scripts...')
         sys.stdin.read()
     except frida.InvalidArgumentError as e:
-        print('[严重] 非法参数，可能是JavaScript脚本存在语法错误，或者设备意外断开连接，详细信息: '+repr(e))
+        print('[Error] Invalid argument, maybe a JavaScript syntax error or device disconnected. Details: '+repr(e))
     except frida.ProcessNotFoundError as e:
-        print('[严重] 进程未找到，请在设备上启动目标应用程序，详细信息: '+repr(e))
+        print('[Error] Cannot find the target process, please check your application status. Details: '+repr(e))
     except frida.ServerNotRunningError as e:
-        print('[严重] 服务端没有运行，请在设备上使用超级用户权限运行frida服务端程序，详细信息: '+repr(e))
+        print('[Error] Frida server is not running, please check if it is not start or crash. Details: '+repr(e))
+    except Exception as e:
+        print('[Error] Unknown error or exception. Details: ' + repr(e))
